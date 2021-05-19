ace0692855b77c7f62f691ccceb7b02f73509415
szanuje <maksple1337@gmail.com>
2021-05-19 15:30:55 +0200

    update docker

--- app/services/ProductService.scala
+++ app/services/ProductService.scala
@@ -3,29 +3,34 @@ package services
 import org.mongodb.scala._
 import org.mongodb.scala.bson.Document
 import org.mongodb.scala.model.Filters
+import play.api.Configuration
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class ProductService @Inject()(implicit ec: ExecutionContext) {
+class ProductService @Inject()(implicit ec: ExecutionContext, config: Configuration) {
+
+  val host = config.underlying.getString("MONGO_HOST")
+  val MONGO_URL = "mongodb://" + host + ":27017"
 
   def findAll(): Future[Seq[Document]] = {
-    val mongoClient: MongoClient = MongoClient("mongodb://localhost:27017")
+    println(host)
+    val mongoClient: MongoClient = MongoClient(MONGO_URL)
     val database: MongoDatabase = mongoClient.getDatabase("shop")
     val collection: MongoCollection[Document] = database.getCollection("products")
     collection.find().toFuture()
   }
 
   def findBy(property: String, queryString: String): Future[Seq[Document]] = {
-    val mongoClient: MongoClient = MongoClient("mongodb://localhost:27017")
+    val mongoClient: MongoClient = MongoClient(MONGO_URL)
     val database: MongoDatabase = mongoClient.getDatabase("shop")
     val collection: MongoCollection[Document] = database.getCollection("products")
     collection.find(Filters.eq(property, queryString)).toFuture()
   }
 
   def insert(product: models.Product): Unit = {
-    val mongoClient: MongoClient = MongoClient("mongodb://localhost:27017")
+    val mongoClient: MongoClient = MongoClient(MONGO_URL)
     val database: MongoDatabase = mongoClient.getDatabase("shop")
     val collection: MongoCollection[Document] = database.getCollection("products")
     val doc: Document = Document(
