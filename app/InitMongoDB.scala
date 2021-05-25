b9d5ef63ea6ce1f9e0a43c118c14ad56acd4d7bf
szanuje <maksple1337@gmail.com>
2021-05-25 19:33:56 +0200

    init db

--- app/InitMongoDB.scala
+++ app/InitMongoDB.scala
@@ -7,40 +7,39 @@ import scala.concurrent.Future
 
 
 @Singleton
-class InitMongoDB @Inject()(lifecycle: ApplicationLifecycle, clientService: ClientService) {
+class InitMongoDB @Inject()(lifecycle: ApplicationLifecycle,
+                            clientService: ClientService,
+                            productService: ProductService) {
 
-  clientService.createClient(Client(
-    User("user2", "pass1", "user1@gmail.com"),
-    UserDetails("address1", "city1", "country1", "00-00", "9999999999"),
-    UserBasket(
-      List(
-        BasketProduct(
-          Product("name1", "category1", "description1", 49.99f),
-          2,
-          99.98f
-        )
-      ),
-      99.9f
-    )
-  ))
-
-  clientService.createClient(Client(
-    User("user1", "pass1", "user1@gmail.com"),
-    UserDetails("address1", "city1", "country1", "00-00", "9999999999"),
-    UserBasket(
-      List(
-        BasketProduct(
-          Product("name1", "category1", "description1", 49.99f),
-          2,
-          99.98f
-        )
-      ),
-      99.9f
-    )
-  ))
+  initDB()
 
   lifecycle.addStopHook { () =>
     println("stop")
     Future.successful(())
   }
+
+  def initDB(): Unit = {
+    for (i <- 0 to 20) {
+      productService.createProduct(Product("name" + i, "category" + i % 5, "description" + i, 9.99f))
+    }
+    val product1 = Product("name1", "category1", "description1", 9.99f)
+    val product2 = Product("name2", "category2", "description2", 9.99f)
+    val product3 = Product("name3", "category3", "description3", 9.99f)
+    val user1 = User("user1", "pass1", "user1@gmail.com")
+    val user2 = User("user2", "pass2", "user2@gmail.com")
+    val userDetails1 = UserDetails("address1", "city1", "country1", "11-00", "111111111")
+    val userDetails2 = UserDetails("address2", "city2", "country2", "22-00", "222222222")
+    val userBasket1 = UserBasket(List(
+      BasketProduct(product1, 2, 19.98f)),
+      19.98f)
+    val userBasket2 = UserBasket(List(
+      BasketProduct(product1, 2, 19.98f),
+      BasketProduct(product2, 1, 9.99f),
+      BasketProduct(product3, 1, 9.99f)),
+      39.96f)
+
+    clientService.createClient(Client(user1, userDetails1, userBasket1))
+    clientService.createClient(Client(user2, userDetails2, userBasket2))
+  }
+
 }
